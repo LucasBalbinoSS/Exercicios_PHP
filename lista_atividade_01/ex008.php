b4d720e9e2182c4651baa95ea3b7e317a3d20afa
Lucas Balbino <lucas.balbinosantos@hotmail.com>
2022-04-22 08:10:18 -0300

    Exercício 08 PHP

---
+++ lista_atividade_01/ex008.php
@@ -0,0 +1,18 @@
+<!-- 
+Faça um programa que leia um número e, caso ele seja positivo, calcule e mostre: 
+
+• O número digitado ao quadrado
+• A raiz quadrada do número digitado
+ -->
+
+<?php
+
+$num1 = 9;
+
+if ($num1 > 0) {
+    echo $num1 ** 2;
+    echo '<br>';
+    echo $num1 / 2;
+}
+
+?>
