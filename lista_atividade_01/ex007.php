8a678687ea45c506af5e8f80507fc3cf6c52579f
Lucas Balbino <lucas.balbinosantos@hotmail.com>
2022-04-22 08:09:43 -0300

    Exercício 07 PHP

---
+++ lista_atividade_01/ex007.php
@@ -0,0 +1,13 @@
+<!-- Faça um programa que leia um número caso seja positivo, calcule a raiz quadrada do número. Caso o númeroseja negativo, mostre uma mensagem dizendo que o númeroé invalido. -->
+
+<?php
+
+$num1 = -4;
+
+if ($num1 > 0) {
+    echo $num1 / 2;
+} else {
+    echo "O número $num1 é um número inválido! por favor, tente novamente";
+}
+
+?>
