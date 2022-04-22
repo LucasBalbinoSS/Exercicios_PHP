dd7f656a78360797aa545ab255b271efc80eed3e
Lucas Balbino <lucas.balbinosantos@hotmail.com>
2022-04-22 08:08:42 -0300

    Exercício 06 PHP

---
+++ lista_atividade_01/ex006.php
@@ -0,0 +1,16 @@
+<?php
+
+$num1 = 10;
+$num2 = 15;
+
+switch ($num1) {
+    case $num1 > $num2:
+        echo "Sim, $num1 é maior que $num2";
+        break;
+    
+    default:
+        echo "O número $num2 é maior que o número $num1";
+        break;
+}
+
+?>
