d93e3ca79c9c3ff357f84ee6f2055df8f7384265
Lucas Balbino <lucas.balbinosantos@hotmail.com>
2022-05-08 00:27:26 -0300

    Exemplo usando o método

---
+++ metodo_get/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Método Get com PHP</title>
+</head>
+<body>
+
+<form method="get" action="recebe_get.php">
+    <input type="text" name="nome" id="" placeholder="Digite seu nome aqui">
+    <br>
+    <br>
+    <input type="text" name="idade" id="" placeholder="Digite sua idade aqui">
+    <hr>
+    <button type="submit">Enviar</button>
+</form>
+
+</body>
+</html>
